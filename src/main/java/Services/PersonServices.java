341861caf3274b10b47131ba0cd934b57e930af9
gabrielggontijo <gabriel.aggf@hotmail.com>
2025-02-05 07:47:11 -0300

    feat: criacao do find All para retornar lista de json, criacao do metodo no controller e no person services

--- src/main/java/Services/PersonServices.java
+++ src/main/java/Services/PersonServices.java
@@ -3,6 +3,8 @@ package Services;
 import Model.Person;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.atomic.AtomicLong;
 import java.util.logging.Logger;
 
@@ -12,17 +14,39 @@ public class PersonServices {
     private final AtomicLong counter = new AtomicLong();
     private Logger logger = Logger.getLogger(PersonServices.class.getName());
 
+    public List<Person> findAll() {
+
+        logger.info("Find all persons");
+
+        List<Person> persons = new ArrayList<>();
+        for (int i = 0; i < 8; i++) {
+            Person person = mockPerson(i);
+            persons.add(person);
+        }
+        return persons;
+    }
+
     public Person findById(String id) {
-        var contadorId = counter.getAndIncrement();
-        logger.info("Find Person by id: " + contadorId);
+
+        logger.info("Find Person by id: " +  id);
 
         Person person = new Person();
-       person.setId(contadorId);
+        person.setId(counter.getAndIncrement());
         person.setFirstName("Gabriel");
         person.setLastName("Ferreira");
         person.setGender("Male");
         person.setAddress("Fluorina, 52");
         return person;
+    }
+    private Person mockPerson(int i) {
+        Person person = new Person();
+        person.setId(counter.getAndIncrement());
+        person.setFirstName("Person name: " + i);
+        person.setLastName("Last name: " + i);
+        person.setGender("Male: " + i);
+        person.setAddress("Some address: " + i);
+        return person;
+
     }
 
 }
--- src/main/java/com/example/demo/PersonController.java
+++ src/main/java/com/example/demo/PersonController.java
@@ -7,19 +7,27 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 //
 @RestController
 @RequestMapping("/person")
 public class PersonController {
 
-    private PersonServices services = new PersonServices();
+    PersonServices services = new PersonServices();
 
     @RequestMapping(value = "/{id}",
             method = RequestMethod.GET,
             produces = MediaType.APPLICATION_JSON_VALUE)
-    public Person findById (@PathVariable(value = "id") String id) {
+    public Person findById(@PathVariable(value = "id") String id) {
         return services.findById(id);
 
+    }
+
+    @RequestMapping(method = RequestMethod.GET,
+            produces = MediaType.APPLICATION_JSON_VALUE)
+    public List<Person> findAll() {
+        return services.findAll();
 
     }
 
